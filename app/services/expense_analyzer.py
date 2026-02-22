de8bf5ee3665876baa1a690133ab92add8b1ee3a
Daksh1111174 <shahdakshinfo@gmail.com>
2026-02-22 10:33:33 +0530

    Update expense_analyzer.py

--- app/services/expense_analyzer.py
+++ app/services/expense_analyzer.py
@@ -1,1 +1,15 @@
+def analyze_expense(df):
+    df["total_expense"] = df.iloc[:, 1:].sum(axis=1)
 
+    total_income = df["income"].sum()
+    total_expense = df["total_expense"].sum()
+
+    savings = total_income - total_expense
+    ratio = savings / total_income if total_income > 0 else 0
+
+    return {
+        "income": total_income,
+        "expense": total_expense,
+        "savings": savings,
+        "ratio": ratio
+    }
