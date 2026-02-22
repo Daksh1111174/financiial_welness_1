dc4ff6b9e6aeea3943a770e9749106e1b95822f3
Daksh1111174 <shahdakshinfo@gmail.com>
2026-02-22 10:34:35 +0530

    Update risk_model.py

--- app/ ml_models/risk_model.py
+++ app/ ml_models/risk_model.py
@@ -1,1 +1,7 @@
-
+def predict_risk(income, savings_ratio, age):
+    if age < 30 and savings_ratio > 0.2:
+        return "High"
+    elif age < 50:
+        return "Moderate"
+    else:
+        return "Low"
