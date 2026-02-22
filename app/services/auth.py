a90de5bff17993788ee4b9f622710cd2bae6c068
Daksh1111174 <shahdakshinfo@gmail.com>
2026-02-22 10:40:39 +0530

    Update auth.py

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,1 +1,41 @@
+import sqlite3
+import hashlib
 
+def init_db():
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            password TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def register_user(username, password):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    try:
+        c.execute("INSERT INTO users VALUES (?, ?)",
+                  (username, hash_password(password)))
+        conn.commit()
+        return True
+    except:
+        return False
+    finally:
+        conn.close()
+
+def login_user(username, password):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("SELECT password FROM users WHERE username=?", (username,))
+    result = c.fetchone()
+    conn.close()
+
+    if result and result[0] == hash_password(password):
+        return True
+    return False
