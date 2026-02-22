99338dcf6e751f34ae819008d9cc41eaa58c6b36
Daksh1111174 <shahdakshinfo@gmail.com>
2026-02-22 10:34:17 +0530

    Update prophet_model.py

--- app/ ml_models/prophet_model.py
+++ app/ ml_models/prophet_model.py
@@ -1,1 +1,16 @@
+from prophet import Prophet
+import pandas as pd
 
+def prophet_forecast(series):
+    df = pd.DataFrame({
+        "ds": pd.date_range("2023-01-01", periods=len(series), freq="M"),
+        "y": series.values
+    })
+
+    model = Prophet()
+    model.fit(df)
+
+    future = model.make_future_dataframe(periods=6, freq="M")
+    forecast = model.predict(future)
+
+    return forecast[["ds","yhat"]].tail(6)
